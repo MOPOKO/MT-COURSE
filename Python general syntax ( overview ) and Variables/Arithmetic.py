ff78050f3b2f62403fc2bac13445c6a7a8417c94
MOPOKO <38830060+MOPOKO@users.noreply.github.com>
2020-06-04 08:11:53 +0600

    Create Arithmetic.py

---
+++ Python general syntax ( overview ) and Variables/Arithmetic.py
@@ -0,0 +1,26 @@
+Addition, subtraction, multiplication, division, and other numeric calculations are easy to do in most programming languages, and Python is no exception. 
+
+Example: 
+
+addition = 2020 + 2020
+
+subtraction = 2020 - 2019
+
+multiplication = 12 * 12
+
+division = 36 / 3
+
+reminder = 15 % 2
+
+
+
+Multiply two numbers a and b together and assign the result to variable called answer, where "a" holds value of today's year, and "b" holds value 1
+
+Then print out answer 
+____________________________________
+case = Test1
+output = 2020
+____________________________________
+a = 2020
+b = 1
+print(a*b)
