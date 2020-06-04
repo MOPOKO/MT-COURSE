cd8524d9c3ee4bad3b0ccef1369b39ff7c7dfc5f
MOPOKO <38830060+MOPOKO@users.noreply.github.com>
2020-06-04 08:09:32 +0600

    Create Today's year.py

---
+++ Python general syntax ( overview ) and Variables/Today's year.py
@@ -0,0 +1,14 @@
+In Python, and when programming in general, we need to build systems for dealing with data that changes over time.
+Python uses variables to define things that are subject to change.
+
+Example: date = 2019
+
+Create a variable called date and assign a value that will represent today’s year to that variable.
+
+Then output value of this variable on a separate line
+____________________________________
+case = Test1
+output = "2020"
+____________________________________
+date = 2020
+print(date) 
