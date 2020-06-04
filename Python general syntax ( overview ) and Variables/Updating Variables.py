eb0e655cdb6db7bc84c2612c0fb89533c5054eaf
MOPOKO <38830060+MOPOKO@users.noreply.github.com>
2020-06-04 08:13:06 +0600

    Create Updating Variables.py

---
+++ Python general syntax ( overview ) and Variables/Updating Variables.py
@@ -0,0 +1,25 @@
+Changing the contents of a variable is one of the essential operations. As the flow of a program progresses, data should be updated to reflect changes that have happened.
+
+
+
+Example:
+
+money_in_wallet = 100
+
+sandwich_price = 30
+
+money_in_wallet = money_in_wallet - sandwich_price
+
+print(money_in_wallet) 
+
+
+
+Copy this code to compiler, to see the answer of this transaction. 
+____________________________________
+case = Test1
+output = 70
+____________________________________
+money_in_wallet = 100
+sandwich_price = 30
+money_in_wallet = money_in_wallet - sandwich_price
+print(money_in_wallet) 
