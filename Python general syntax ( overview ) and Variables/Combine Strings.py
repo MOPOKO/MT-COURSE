0bfee329114ba95709725d34c2d22b1f3fed17fc
MOPOKO <38830060+MOPOKO@users.noreply.github.com>
2020-06-04 08:10:45 +0600

    Create Combine Strings.py

---
+++ Python general syntax ( overview ) and Variables/Combine Strings.py
@@ -0,0 +1,17 @@
+A string, so named because they’re a series of letters, numbers, or symbols connected in order — as if threaded together by string.
+
+Example: "IAU2020" or "2020" or "IAU"
+
+Deploy two variables which holds values "a" and "b", where "a" equals to the 2020, and "b" equals "a2" respectively.
+
+And using "+" operator, output two values near to each other on the new line.
+
+Attention: You work with strings not integers
+____________________________________
+case = Test 1
+output = "20204080400"
+____________________________________
+a = "2020"
+b = "4080400"
+ 
+print(a+b) 
