f247cb9a777cb78503b1f99f21d3ced008703a13
MOPOKO <38830060+MOPOKO@users.noreply.github.com>
2020-06-04 08:14:15 +0600

    Create Updating Variables 2.py

---
+++ Python general syntax ( overview ) and Variables/Updating Variables 2.py
@@ -0,0 +1,24 @@
+hanging the contents of a variable is one of the essential operations. As the flow of a program progresses, data should be updated to reflect changes that have happened.
+
+Example:
+
+money_in_wallet = 100
+
+sandwich_price = 30
+
+money_in_wallet -= sandwich_price
+
+print(money_in_wallet) 
+
+
+In this case, we see, that same procedure can be shortened using -= or += instead writing whole procedure again.
+
+Now copy the code above, and see the output in this case.
+____________________________________
+case = Test1
+output = 70
+____________________________________
+money_in_wallet = 100
+sandwich_price = 30
+money_in_wallet -= sandwich_price
+print(money_in_wallet)  
