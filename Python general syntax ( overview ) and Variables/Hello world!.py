a27c3531efd3553c84071e7dd708ecb955b4b095
MOPOKO <38830060+MOPOKO@users.noreply.github.com>
2020-06-04 08:06:40 +0600

    Create Hello world!.py

---
+++ Python general syntax ( overview ) and Variables/Hello world!.py
@@ -0,0 +1,11 @@
+A print statement is the easiest way to get your Python program to communicate with you. Being able to command this communication will be one of the most valuable tools in your programming toolbox.
+
+Example:
+print("Machine Translation Course")
+
+Using a print statement, output a "Hello, World!"
+____________________________________
+case = Test 1
+output = "Hello, World!"
+____________________________________
+print("Hello, World!") 
