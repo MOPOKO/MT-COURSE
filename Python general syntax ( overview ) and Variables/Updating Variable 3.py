376f6b57c3f63e091b30c3264fa807f33e55a07c
MOPOKO <38830060+MOPOKO@users.noreply.github.com>
2020-06-04 08:15:25 +0600

    Create Updating Variable 3.py

---
+++ Python general syntax ( overview ) and Variables/Updating Variable 3.py
@@ -0,0 +1,13 @@
+Changing the contents of a variable is one of the essential operations. As the flow of a program progresses, data should be updated to reflect changes that have happened.
+
+Now imagine the situation from past examples, but instead of buying a sandwich, you meet a friend and he will return you a debt, the money that he borrowed from you
+
+Declare variables money_in_wallet, money_borrowed, where money_in_wallet holds value 100 and money_borrowed holds value 50, then output total amount of money you gained
+____________________________________
+case = Test1
+output = 150
+____________________________________
+money_in_wallet = 100
+money_borrowed = 50
+money_in_wallet += money_borrowed
+print(money_in_wallet)
